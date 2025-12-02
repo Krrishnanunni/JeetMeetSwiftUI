64eacb2f116c2d506d15ed6c6062cd88967421e0
Krrishnanunni <krishnanunni2004ka@gmail.com>
2025-12-03 01:44:07 +0530

    Dashboard card

---
+++ JeetMeetSwiftUI/LoginPage.swift
@@ -0,0 +1,18 @@
+//
+//  LoginPage.swift
+//  JeetMeetSwiftUI
+//
+//  Created by Krishnanunni K A on 02/12/25.
+//
+
+import SwiftUI
+
+struct LoginPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginPage()
+}
